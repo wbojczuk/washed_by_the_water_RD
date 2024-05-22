3fad84e7f2d3792d45a3430b4ce9688f54d0fff3
William Bojczuk <williambojczuk@gmail.com>
2024-05-21 23:40:56 -0400

    updated

--- src/app/(mainsite)/components/homepage/HorizontalFreeQuoteForm/HorizontalFreeQuoteForm.tsx
+++ src/app/(mainsite)/components/homepage/HorizontalFreeQuoteForm/HorizontalFreeQuoteForm.tsx
@@ -29,7 +29,7 @@ export default function HorizontalFreeQuoteForm() {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="rgb(200, 200, 200)" d="M15 12h2a5 5 0 0 0-5-5v2a3 3 0 0 1 3 3m4 0h2c0-5-4.03-9-9-9v2c3.86 0 7 3.13 7 7m1 3.5c-1.25 0-2.45-.2-3.57-.57c-.35-.11-.74-.03-1.02.25l-2.2 2.2a15.097 15.097 0 0 1-6.59-6.59l2.2-2.2c.28-.28.36-.67.25-1.02A11.36 11.36 0 0 1 8.5 4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1a17 17 0 0 0 17 17a1 1 0 0 0 1-1v-3.5a1 1 0 0 0-1-1"></path></svg>
                 </div>
                 <div className={styles.inputWrapper}>
-                    <input required type="email" name="email" id="emailInput" placeholder="Email" />
+                    <input required type="email" name="Email" id="emailInput" placeholder="Email" />
 
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#c8c8c8" d="m20 8l-8 5l-8-5V6l8 5l8-5m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"></path></svg>
                 </div>
